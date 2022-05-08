61457f9acdfe9b1d2bf66fa6b4cbb4b3a5dee418
Erick Vargas <erickkvargas@gmail.com>
2022-05-08 13:08:30 -0700

    commit tree aa158ff5f3239c89a5b33c2e90c4cf2e09702514

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter} from "react-router-dom";
+import App from "./App";
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
 
-function App() {
-  return <Home />;
-}
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+
+
+
+
+
+
+
+//use <route> components to define clinet-side router in our application 
+//router component is in charge of sayinmg when the URL matches this psecified path render this compnent 
+//router component has one prop on our example: Path 
+
+//use <browserRouter> to wrap entire application and provide react router context features 
+ReactDOM.render(
+<BrowserRouter>
+    <App/>
+</BrowserRouter>, 
+document.getElementById("root"));
